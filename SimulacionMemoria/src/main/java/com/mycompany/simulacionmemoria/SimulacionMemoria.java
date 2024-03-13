93dd408c62d722ece9ece802b0c1c911cc8bf3fd
EUgeneWu11 <genewu013@gmail.com>
2024-03-12 18:25:26 -0600

    Primer Commit
    Primer avance proyecto

---
+++ SimulacionMemoria/src/main/java/com/mycompany/simulacionmemoria/SimulacionMemoria.java
@@ -0,0 +1,15 @@
+
+
+package com.mycompany.simulacionmemoria;
+
+/**
+ *
+ * @author HD
+ */
+public class SimulacionMemoria {
+
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.setVisible(true);
+    }
+}
