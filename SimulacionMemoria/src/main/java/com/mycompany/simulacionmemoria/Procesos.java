d92f58dad670996706fd7aaf9bc7882a1fb2115a
Eugenewu11 <genewu013@gmail.com>
2024-04-05 01:14:28 -0600

    Creacion clase Particion

--- SimulacionMemoria/src/main/java/com/mycompany/simulacionmemoria/Procesos.java
+++ SimulacionMemoria/src/main/java/com/mycompany/simulacionmemoria/Procesos.java
@@ -3,14 +3,19 @@ package com.mycompany.simulacionmemoria;
 
 //Aqui van los imports
 
+import javax.swing.Timer;
+
+
 
 public class Procesos {
     private String nombreProceso;
     private int idProceso;
+    private int Estado;
     private int memoriaRequerida;
     private int tiempoRequerido;
     private int duracionProceso;
-    
+    private int actTiempo;
+    private Timer timer;
      // Constructor
     public Procesos(String nombreProceso, int idProceso, String estadoProceso, int memoriaRequerida, 
             int tiempoRequerido, int duracionProceso){
@@ -21,7 +26,6 @@ public class Procesos {
         this.tiempoRequerido = tiempoRequerido;
         this.duracionProceso = duracionProceso;
     }
-
     // Getters y setters
     public String getNombreProceso() {
         return nombreProceso;
